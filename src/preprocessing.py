2fc14078a35334ec14d323b1b1b2f6a55fe99096
Abderrahmane Kadmiri <kadmiri10@outlook.com>
2026-09-15 20:28:40 +0100

    CReate a preprocessing file to drop non significant variables found in the exploration notebook

---
+++ src/preprocessing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from config import CLEAN_INSCRIPTIONS, FINAL_INSCRIPTIONS
+
+
+# Drop non significant variables according to the descriptive stats notebook
+data = pd.read_csv(CLEAN_INSCRIPTIONS)
+insignificant = ['genre','cohorte']
+redundant_cols = ['id_programme', 'resultat_final']
+cols_to_drop = insignificant + redundant_cols
+data.drop(cols_to_drop,axis='columns', inplace=True)
+data.to_csv(FINAL_INSCRIPTIONS, index=False)
