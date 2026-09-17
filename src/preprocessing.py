090762e5c3542d0c44ad8ee535950575a967c145
Abderrahmane Kadmiri <kadmiri10@outlook.com>
2026-09-17 23:19:11 +0100

    Update the preprocessing script

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,7 +5,8 @@ from config import CLEAN_INSCRIPTIONS, FINAL_INSCRIPTIONS
 # Drop non significant variables according to the descriptive stats notebook
 data = pd.read_csv(CLEAN_INSCRIPTIONS)
 insignificant = ['genre','cohorte']
-redundant_cols = ['id_programme', 'resultat_final']
-cols_to_drop = insignificant + redundant_cols
+redundant_cols = ['id_programme','resultat_final','date_debut','date_annulation', ]
+missing_cols = ['avg_note','avg_lateness_days']
+cols_to_drop = insignificant + redundant_cols + missing_cols
 data.drop(cols_to_drop,axis='columns', inplace=True)
 data.to_csv(FINAL_INSCRIPTIONS, index=False)
